7c4fb17b8c3a4cde42e10b9ae2c9043319ea1def
Marco Dempsey Wright <wrigh4md@mail.uc.edu>
2025-03-02 15:15:01 -0500

    final commit

--- src/main.java
+++ src/main.java
@@ -6,8 +6,11 @@ public class main {
 
         String player1;
         String player2;
+        int player1count = 0;
+        int player2count = 0;
         boolean valid = false;
         boolean done = false;
+        String playing;
         do {
             do {
                 System.out.print("what is player 1s move[R, P, S]:");
@@ -41,17 +44,80 @@ public class main {
             }
             while (!valid);
 
-            if (player1.equalsIgnoreCase("R") && player2.equalsIgnoreCase("S") || player1.equalsIgnoreCase("P"))
+            if (player1.equalsIgnoreCase("R") && player2.equalsIgnoreCase("S")){
 
+                System.out.print("Rock beats Scissors, Player 1 wins\n");
+                player1count ++;
+                System.out.print("player one score:" + player1count);
+                System.out.print("\nplayer two score:" + player2count);
+            }
+            else if (player1.equalsIgnoreCase("P") && player2.equalsIgnoreCase("R")){
 
+                System.out.print("Paper beats Rock, Player 1 wins\n");
+                player1count ++;
+                System.out.print("player one score:" + player1count);
+                System.out.print("\nplayer two score:" + player2count);
+            }
+            else if (player1.equalsIgnoreCase("S") && player2.equalsIgnoreCase("P")){
 
+                System.out.print("Scissors beats Paper, Player 1 wins\n");
+                player1count ++;
+                System.out.print("player one score:" + player1count);
+                System.out.print("\nplayer two score:" + player2count);
+            }
+            else if (player2.equalsIgnoreCase("R") && player1.equalsIgnoreCase("S")){
 
+                System.out.print("Rock beats Scissors, Player 2 wins\n");
+                player2count ++;
+                System.out.print("player one score:" + player1count);
+                System.out.print("\nplayer two score:" + player2count);
+            }
+            else if (player2.equalsIgnoreCase("P") && player1.equalsIgnoreCase("R")){
 
+                System.out.print("Paper beats Rock, Player 2 wins\n");
+                player2count ++;
+                System.out.print("player one score:" + player1count);
+                System.out.print("\nplayer two score:" + player2count);
+            }
+            else if (player2.equalsIgnoreCase("S") && player1.equalsIgnoreCase("P")){
 
+                System.out.print("Scissors beats Paper, Player 2 wins\n");
+                player2count ++;
+                System.out.print("player one score:" + player1count);
+                System.out.print("\nplayer two score:" + player2count);
+            }
+            else if (player2.equalsIgnoreCase("R") && player1.equalsIgnoreCase("R")){
 
+                System.out.print("Rock vs Rock, its a tie\n");
 
+                System.out.print("player one score:" + player1count);
+                System.out.print("\nplayer two score:" + player2count);
+            }
+            else if (player2.equalsIgnoreCase("P") && player1.equalsIgnoreCase("P")){
 
+                System.out.print("Paper vs Paper, its a tie\n");
+                System.out.print("player one score:" + player1count);
+                System.out.print("\nplayer two score:" + player2count);
+            }
+            else if (player2.equalsIgnoreCase("S") && player1.equalsIgnoreCase("S")){
 
+                System.out.print("Scissors vs Scissors, its a tie\n");
+                System.out.print("player one score:" + player1count);
+                System.out.print("\nplayer two score:" + player2count);
+            }
+
+            System.out.print("\ndo you want to keep playing?[Y,N]");
+            playing = in.next();
+
+            if (playing.equalsIgnoreCase("Y")){
+
+                done = false;
+            }
+            else if (playing.equalsIgnoreCase("N")){
+                System.out.print("thank you for playing!");
+                done = true;
+
+            }
         }
         while (!done);
     }
